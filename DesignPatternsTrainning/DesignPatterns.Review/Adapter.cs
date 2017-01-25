c74bc9496be14271425a8eecdcf5659f8d3ccd3f
Franz <franzhflores@hotmail.com>
2017-01-25 00:00:40 -0400

    try adapter

---
+++ DesignPatternsTrainning/DesignPatterns.Review/Adapter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Review
+{
+
+    interface IWriterMicroSD
+    {
+        int NumberOfPins { get; set; }
+        // IMicroSD microSD { get; set; }
+        void Read();
+
+        void Insert(IMicroSD microSD);
+
+    }
+
+
+    class WriterMicroSD_Samsung : IWriterMicroSD
+    {
+
+        int _numberOfPins;
+        public int NumberOfPins
+        {
+            get
+            {
+                return _numberOfPins;
+            }
+
+            set
+            {
+                this._numberOfPins = value;
+            }
+        }
+
+        public void Insert(IMicroSD microSD)
+        {
+            if (microSD.NumberOfPins == this._numberOfPins)
+                Console.WriteLine("Contains a MICROSD");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Ready for reading!");
+        }
+    }
+
+
+    interface IMicroSD
+    {
+
+        int NumberOfPins { get; set; }
+        string Model {get; set;}
+        void Format();
+    }
+
+    public class Adapter
+    {
+
+    }
+}
